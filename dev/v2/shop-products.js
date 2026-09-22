394c1cadcbd666d2aa99578004c5a15c603c4ab1
Geepard86 <119399773+Geepard86@users.noreply.github.com>
2026-09-22 06:55:03 +0200

    Update shop-products.js

--- dev/v2/shop-products.js
+++ dev/v2/shop-products.js
@@ -39,15 +39,15 @@
   const SHOP_PAGE = "shop.html";
 
   const PRODUCTS = [
-  { id: "T2372A2PA7710PT17X15Y2D360161482W34913H41896", name: "Lasse - The Playcaller", preis: "32,49 €", startToken: playcaller-A6aa8d5135af448088cbc409c?sellable=lNQewEynOxf300ejaxRG-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X40Y5D360161468W29900H44850", name: "Tobi - The Wildcard", preis: "32,49 €", startToken: wildcard-A6aa8d05ee66f06035604722a?sellable=v9VllvXxJkF8JY83vE3d-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X40Y16D360155352W29899H44849", name: "Markus - The Cold Blooded", preis: "32,49 €", startToken: coldblooded-A6aa7edf9e66f0603564c94f4?sellable=Ra5p5wXDqdUnN2nqx2zV-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X40Y16D360155245W29900H44850", name: "Micha - The Chaos Factor", preis: "32,49 €", startToken: chaosfactor-A6aa7edf9af48c111d2ab25fa?sellable=VMq1qOgxAvTbaXYr40zv-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X40Y16D360155286W29900H44850", name: "Alex - The Contender", preis: "32,49 €", startToken: contender-A6aa7edf95af448088cf42320?sellable=Ab8r85eg80frZZ5YG3Vz-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X15Y31D360155248W34899H41880", name: "The Rookie Class", preis: "32,49 €", startToken: rookieclass-A6aa7edf9e66f0603564c8ee6?sellable=pNVRVLQj8quvomvM2mE1-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X15Y8D360155311W34998H36743", name: "Tobi - The Mouth", preis: "32,49 €", startToken: mouth-A6aa7edf95af448088cf41d12?sellable=9Oz4zEQgz0S7xxLR9lxp-2372-8&appearance=2 },
-  { id: "T2372A2PA7710PT17X15Y11D360155283W34913H41896", name: "Tim - The Defender", preis: "32,49 €", startToken: defender-A6aa7edf9af48c111d2ab25eb?sellable=oNVGVn7gybSkMG1a7JGJ-2372-8&appearance=2" },
-  { id: "T2372A2PA7711PT17X15Y10D360155285W34900H23266", name: "Altima Bowl VI", preis: "32,49 €", view: 2, startToken: altimabowl6-A6aa7edf75af448088cf416ed?sellable=XNpkpGevgnSBGGM1eN19-2372-8&appearance=2, view: "2" },
+  { id: "T2372A2PA7710PT17X15Y2D360161482W34913H41896", name: "Lasse - The Playcaller", preis: "32,49 €", startToken: "playcaller-A6aa8d5135af448088cbc409c?sellable=lNQewEynOxf300ejaxRG-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X40Y5D360161468W29900H44850", name: "Tobi - The Wildcard", preis: "32,49 €", startToken: "wildcard-A6aa8d05ee66f06035604722a?sellable=v9VllvXxJkF8JY83vE3d-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X40Y16D360155352W29899H44849", name: "Markus - The Cold Blooded", preis: "32,49 €", startToken: "coldblooded-A6aa7edf9e66f0603564c94f4?sellable=Ra5p5wXDqdUnN2nqx2zV-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X40Y16D360155245W29900H44850", name: "Micha - The Chaos Factor", preis: "32,49 €", startToken: "chaosfactor-A6aa7edf9af48c111d2ab25fa?sellable=VMq1qOgxAvTbaXYr40zv-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X40Y16D360155286W29900H44850", name: "Alex - The Contender", preis: "32,49 €", startToken: "contender-A6aa7edf95af448088cf42320?sellable=Ab8r85eg80frZZ5YG3Vz-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X15Y31D360155248W34899H41880", name: "The Rookie Class", preis: "32,49 €", startToken: "rookieclass-A6aa7edf9e66f0603564c8ee6?sellable=pNVRVLQj8quvomvM2mE1-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X15Y8D360155311W34998H36743", name: "Tobi - The Mouth", preis: "32,49 €", startToken: "mouth-A6aa7edf95af448088cf41d12?sellable=9Oz4zEQgz0S7xxLR9lxp-2372-8&appearance=2" },
+  { id: "T2372A2PA7710PT17X15Y11D360155283W34913H41896", name: "Tim - The Defender", preis: "32,49 €", startToken: "defender-A6aa7edf9af48c111d2ab25eb?sellable=oNVGVn7gybSkMG1a7JGJ-2372-8&appearance=2" },
+  { id: "T2372A2PA7711PT17X15Y10D360155285W34900H23266", name: "Altima Bowl VI", preis: "32,49 €", view: 2, startToken: "altimabowl6-A6aa7edf75af448088cf416ed?sellable=XNpkpGevgnSBGGM1eN19-2372-8&appearance=2"},
   ];
 
   function getFeaturedProducts(count) {
